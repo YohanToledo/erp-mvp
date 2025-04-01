699450730c05ff5d4cb45417ecd34f62dab1bb0f
Yohan <yohan.gabriel.bernart@gmail.com>
2025-04-01 00:44:21 -0300

    -

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -2,10 +2,10 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   MODE: z.enum(['production', 'development', 'test']).optional(),
-  VITE_SESSION_KEY: z.string().optional(),
-  VITE_API_URL: z.string().optional(),
-  VITE_ENABLE_API_DELAY: z.string().transform(value => value === 'true').optional(),
-  VITE_SOFTWARE_VERSION: z.string().optional(),
+  VITE_SESSION_KEY: z.string().default('token'),
+  VITE_API_URL: z.string().default('http://localhost:3000'),
+  VITE_ENABLE_API_DELAY: z.string().transform(value => value === 'true').default('true'),
+  VITE_SOFTWARE_VERSION: z.string().default('0.0.1'),
 })
 
 export const env = envSchema.parse(import.meta.env)
