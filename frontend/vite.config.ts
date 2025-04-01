9b5bc46257f1985809022a1e7d70209518bb64b4
Yohan <yohan.gabriel.bernart@gmail.com>
2025-04-01 00:32:50 -0300

    -

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,13 @@ export default defineConfig({
     setupFiles: ['./test/setup.ts'],
     environment: 'happy-dom',
   },
-  base: 'erp-mvp'
+  base: '/erp-mvp',
+  define: {
+    'import.meta.env.VITE_SESSION_KEY': JSON.stringify(process.env.VITE_SESSION_KEY),
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+    'import.meta.env.VITE_ENABLE_API_DELAY': JSON.stringify(process.env.VITE_ENABLE_API_DELAY),
+    'import.meta.env.VITE_SOFTWARE_VERSION': JSON.stringify(process.env.VITE_SOFTWARE_VERSION),
+  },
 } as UserConfig & {
   test: InlineConfig
 })
